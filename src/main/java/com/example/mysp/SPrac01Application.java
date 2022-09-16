c26a5cd86d975ca450d242bd4d71de7bd255c378
=문도연 <muinise74@naver.com>
2022-09-16 09:51:50 +0900

    Initial commit

---
+++ src/main/java/com/example/mysp/SPrac01Application.java
@@ -0,0 +1,13 @@
+package com.example.mysp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPrac01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SPrac01Application.class, args);
+    }
+
+}
